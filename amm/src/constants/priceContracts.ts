d25084c94c72c3f76123aa1732611575393a39c2
Lyu Jinhang <truepai415@gmail.com>
2021-07-09 17:27:01 +0800

    add amm

---
+++ amm/src/constants/priceContracts.ts
@@ -0,0 +1,9 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, bnbAddress: string, lpSatoBnbAddress:string, lpBnbBusdAddress: string} = {
+  cakeAddress: '0x8B998619efb0CF7B3fDc85FF2b8bdDCd694d19c2',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  bnbAddress: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  lpSatoBnbAddress: '0xDC656444FC1701ffF3Fc39Ec8D6379886FecFE6E',
+  lpBnbBusdAddress: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+}
+
+export default priceContracts
---
+++ amm/src/hooks/useGetPriceData.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import BigNumber from 'bignumber.js'
+import { useMulticallContract } from './useContract'
+import ERC20_INTERFACE from '../constants/abis/erc20'
+import priceContracts from '../constants/priceContracts'
+
+type ApiResponse = {
+  prices: {
+    [key: string]: string
+  }
+  update_at: string
+}
+
+/**
+ * Due to Cors the api was forked and a proxy was created
+ * @see https://github.com/pancakeswap/gatsby-pancake-api/commit/e811b67a43ccc41edd4a0fa1ee704b2f510aa0ba
+ */
+const api = 'https://api.pancakeswap.com/api/v1/price'
+
+const useGetPriceData = () => {
+  const [data, setData] = useState<number>(0)
+
+  const multicallContract = useMulticallContract();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if(multicallContract){
+          const {cakeAddress, busdAddress, bnbAddress, lpSatoBnbAddress, lpBnbBusdAddress} = priceContracts;
+          const calls = [
+            [cakeAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpSatoBnbAddress])],
+            [bnbAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpSatoBnbAddress])],
+            [bnbAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpBnbBusdAddress])],
+            [busdAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpBnbBusdAddress])],
+          ];
+
+          const [resultsBlockNumber, result] = await multicallContract.aggregate(calls);
+          const [cakeAmount, bnbAmount1, bnbAmount2, busdAmount] = result.map(r=>{
+            if( r === '0x' ) return 0;
+            return ERC20_INTERFACE.decodeFunctionResult("balanceOf", r)
+          });
+          const cake = new BigNumber(cakeAmount);
+          const bnb1 = new BigNumber(bnbAmount1);
+          const bnb2 = new BigNumber(bnbAmount2);
+          const busd = new BigNumber(busdAmount);
+          const cakePrice = bnb1.div(cake).times(busd.div(bnb2)).toNumber();
+          setData(cakePrice)
+        }
+      } catch (error) {
+        console.error('Unable to fetch price data:', error)
+      }
+    }
+
+    fetchData()
+  }, [multicallContract])
+
+  return data
+}
+
+export default useGetPriceData
